b6e40a13cad85542e232bdef3bea3764b62377a7
arafay493 <arafay493@gmail.com>
2024-07-19 12:01:06 +0500

    GuestScreen Half Done. Masjid Data Dynamically Coming from the FlatList. Now Working on the Map.

--- src/components/GuestScreenComponents/NearByMasjidCard.js
+++ src/components/GuestScreenComponents/NearByMasjidCard.js
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-const NearByMasjidCard = () => {
+const NearByMasjidCard = ({item}) => {
   return (
-    <View>
-      <Text>NearByMasjidCard</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        {/* <Image source={require(`${item.ImageSource}`)} style={styles.image} /> */}
+        {/* <Image source={{ uri: item.ImageSource }} style={styles.image} /> */}
+        <Image source={require('../../assets/images/Masjid.png')} style={styles.image} />
+      </View>
+      <View style={styles.textContainer}>
+        <View>
+          <Text style={styles.textBlack}>{item.name}</Text>
+          <Text style={styles.textBlack}>{item.location}</Text>
+        </View>
+        <View>
+          <Text style={styles.textTheme}>Show on Map</Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default NearByMasjidCard
+export default NearByMasjidCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    backgroundColor: '#F6F6F6',
+    borderRadius: 30,
+    marginBottom: 10,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  imageContainer: {},
+  textContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    // borderWidth: 1,
+  },
+  image: {
+    // height: '100%',
+    // width: '100%',
+  },
+  textBlack: {
+    color: '#000',
+    fontFamily: 'InknutAntiqua-SemiBold',
+    fontSize: 10,
+  },
+  textTheme: {
+    color: 'rgba( 118 ,158 ,209 , 0.85)',
+    fontFamily: 'InknutAntiqua-SemiBold',
+    fontSize: 10,
+    textDecorationLine: 'underline',
+    marginBottom: 5,
+  },
+});
--- src/components/GuestScreenComponents/NearBySection.js
+++ src/components/GuestScreenComponents/NearBySection.js
@@ -1,12 +1,57 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
 import React from 'react';
+import NearByMasjidCard from './NearByMasjidCard';
+
+const DATA = [
+  {
+    id: '1',
+    name: 'Quba Masjid',
+    location: 'Karachi',
+    showMapLink: '',
+    imageSource: require('../../assets/images/Masjid.png'),
+  },
+  {
+    id: '2',
+    name: 'Minara Masjid',
+    location: 'Hyderabad',
+    showMapLink: '',
+    imageSource: require('../../assets/images/Masjid.png'),
+  },
+  {
+    id: '3',
+    name: 'Roshan Masjid',
+    location: 'Hyderabad',
+    showMapLink: '',
+    imageSource: require('../../assets/images/Masjid.png'),
+  },
+  {
+    id: '4',
+    name: 'Faisal Masjid',
+    location: 'Faislabad',
+    showMapLink: '',
+    imageSource: require('../../assets/images/Masjid.png'),
+  },
+];
 
 const NearBySection = () => {
   return (
     <View style={styles.container}>
       <View style={styles.nextPrayerBox}>
-        <Text style={[styles.textNextPrayer , styles.textWhite]}>Next Prayer</Text>
-        <Text style={[styles.textTime , styles.textWhite]}>12:52 pm</Text>
+        <Text style={[styles.textNextPrayer, styles.textWhite]}>
+          Next Prayer
+        </Text>
+        <Text style={[styles.textTime, styles.textWhite]}>12:52 pm</Text>
+      </View>
+      <View style={styles.masjidNearByBox}>
+        <Text style={[styles.masjidNearByText, styles.textTheme]}>
+          Masjid's Near by
+        </Text>
+        <FlatList
+          data={DATA}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <NearByMasjidCard item={item} />}
+        /> 
+        {/* <NearByMasjidCard /> */}
       </View>
     </View>
   );
@@ -19,18 +64,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFF',
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
-    paddingHorizontal: 5,
+    paddingHorizontal: 10,
     paddingVertical: 10,
-    marginBottom: 20,
+    height: 430,
   },
   nextPrayerBox: {
     backgroundColor: 'rgba( 118 ,158 ,209 , 0.85)',
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
-    paddingHorizontal: 10,
+    paddingHorizontal: 20,
     paddingVertical: 10,
     marginBottom: 20,
-    elevation: 10
+    // elevation: 10,
   },
   textNextPrayer: {
     fontFamily: 'InknutAntiqua-SemiBold',
@@ -38,9 +83,25 @@ const styles = StyleSheet.create({
   },
   textTime: {
     fontFamily: 'InknutAntiqua-Medium',
-    fontSize: 30
+    fontSize: 30,
+  },
+  masjidNearByBox: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  masjidNearByText: {
+    fontFamily: 'InknutAntiqua-SemiBold',
+    fontSize: 12,
+    marginBottom: 10,
+    marginLeft: 10,
   },
   textWhite: {
     color: '#FFFFFF',
+  },
+  textBlack: {
+    color: '#000000',
+  },
+  textTheme: {
+    color: 'rgba( 118 ,158 ,209 , 0.85)',
   },
 });
